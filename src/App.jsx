eb3843601e8eae51331c87a3fb8a5e2e1d837c98
Moneeba Arshed <moneebaarshed@gmail.com>
2025-03-18 14:21:27 +0500

    setting page

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,12 @@ import Main from "./assets/component/Main/Main"
 function App() {
   return (
     <div className="flex bg-[#F2F4F7] h-screen">
-      
       <Sidebar />
-
-    
       <div className="flex-1 flex flex-col">
         <div className="my-2 me-2">
         <Navbar />
         <Main/>
         </div>
-  
-      
       </div>
     </div>
   );
--- src/assets/component/Main/Main.jsx
+++ src/assets/component/Main/Main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Setting from '../Setting/Setting'
 
 function Main() {
   return (
     <div>
       <div className=" flex items-center justify-between bg-white px-4 shadow-sm border-b my-2 rounded-md"> 
-          <h1 className="text-2xl ">Main Content Goes Here lorem100</h1>
+          <Setting/>
         </div>
     </div>
   )
